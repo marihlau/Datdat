deab5ce97cdd16505ecc11d0720a55184e504af9
Chrisvak <chrisvakrstud.ntnu.no>
2024-03-15 13:30:27 +0100

    initial commit

---
+++ databaseScript.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+db_file = "test.db"  # Legg til riktig filtype for SQLite-databasen
+
+conn = sqlite3.connect(db_file)
+cursor = conn.cursor()
+
+# Endret 'birthday' til 'birthdate' for bedre lesbarhet
+# cursor.execute('''CREATE TABLE person
+#                   (id INTEGER PRIMARY KEY, name TEXT, birthdate TEXT)''')
+
+cursor.execute('''INSERT INTO person VALUES (3, 'Ola Nordmann', '2002-02-02')''')
+
+conn.commit()
+conn.close()
+
+
+# from sqlite3 import Error
+
+# def create_connection(db_file):
+
+#     conn = None
+#     try:
+#         conn = sqlite3.connect(db_file)
+#         print("noe skjedde")
+
+#         cursor = conn.cursor()
+#         cursor.execute( )
+
+    
+#     except Error as e:
+#         print("Funka ikke")
+#     finally:
+#         if conn:
+#             conn.commit()
+#             conn.close()
+
+
+# if __name__ == '__main__':
+#     create_connection(db_file)
