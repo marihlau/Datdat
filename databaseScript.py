710e3514cab59e1a5e0926d90e18ca6a11d94cf1
Chrisvak <chrisvakrstud.ntnu.no>
2024-03-15 13:38:09 +0100

    fiksa

--- databaseScript.py
+++ databaseScript.py
@@ -5,9 +5,7 @@ db_file = "test.db"  # Legg til riktig filtype for SQLite-databasen
 conn = sqlite3.connect(db_file)
 cursor = conn.cursor()
 
-# Endret 'birthday' til 'birthdate' for bedre lesbarhet
-# cursor.execute('''CREATE TABLE person
-#                   (id INTEGER PRIMARY KEY, name TEXT, birthdate TEXT)''')
+cursor.execute('''CREATE TABLE person (id INTEGER PRIMARY KEY, name TEXT, birthday DATE)''')
 
 cursor.execute('''INSERT INTO person VALUES (3, 'Ola Nordmann', '2002-02-02')''')
 
