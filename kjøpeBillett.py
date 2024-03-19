b52b7dadbe43f6beed9eafcad28b620925656725
Chrisvak <chrisvakrstud.ntnu.no>
2024-03-19 17:34:26 +0100

    fiks

--- kjøpeBillett.py
+++ kjøpeBillett.py
@@ -5,8 +5,8 @@ db_file = "test.db"  # Legg til riktig filtype for SQLite-databasen
 conn = sqlite3.connect(db_file)
 cursor = conn.cursor()
 
-def findAvalibleSeats(forestilling, dato, antallSeter):
-    stykkeid = cursor.execute('''SELECT stykkeID FROM akt WHERE navn = ?''' ,(forestilling,))
+def findAvalibleSeats(stykkeNavn, dato, antallSeter):
+    stykkeid = cursor.execute('''SELECT stykkeID FROM teaterStykke WHERE navn = ?''' ,(stykkeNavn,))
     forestillingID = cursor.execute('''SELECT forestillingID FROM forestilling WHERE dato = ? AND stykkeID = ?''', (dato, stykkeid,))
     plassid = cursor.execute('''SELECT plassID FROM billett WHERE forestillingID = ?''', (forestillingID,))
 
